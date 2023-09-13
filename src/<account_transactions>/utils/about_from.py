40831780b3bff0fd09caa8b1f9af05a83c613780
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-14 02:57:12 +0700

    new function added

---
+++ src/<account_transactions>/utils/about_from.py
@@ -0,0 +1,9 @@
+def about_from(data):
+    for element in data:
+        source = element.get('from', 'Investor')
+        if source != 'Investor':
+            account_out = source.split()
+            acc_out_num = account_out[-1]
+            account_out[-1] = f'{acc_out_num[:4]} {acc_out_num[4:6]}** **** {acc_out_num[-4:]}'
+            source = ' '.join(account_out)
+        return source
