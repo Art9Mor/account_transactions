728b7d649f50ef7436d8048cb2c6bc63ddfa6c5d
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-18 01:29:26 +0700

    some cosmetic changes

--- src/<account_transactions>/main.py
+++ src/<account_transactions>/main.py
@@ -2,7 +2,7 @@ from src.utils.cleaned_data import cleaned_data
 from src.utils.from_json import from_json
 from src.utils.last_values import last_values
 from config import OPERATIONS_COUNT
-from src.utils.operations_visor import operation_visor
+from src.utils.operations_visor import operations_visor
 
 
 def main():
@@ -11,7 +11,7 @@ def main():
     data = last_values(data, OPERATIONS_COUNT)
 
     for element in data:
-        print(operation_visor(element), end='\n\n')
+        print(operations_visor(element), end='\n\n')
 
 
 if __name__ == '__main__':
