67777fb753d981a5c13ec2742f4d25f1f3aca388
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-18 04:28:41 +0700

    path changing

--- src/utils/cleaned_data.py
+++ src/utils/cleaned_data.py
@@ -1,3 +1,8 @@
-def cleaned_data(data):
+def cleaned_data(data: list) -> list:
+    """
+    Make a list with operations with executed state
+    :param data: list of dicts with operations
+    :return: operations with executed state
+    """
     data = [i for i in data if 'state' in i and i['state'] == 'EXECUTED']
     return data
